242eafda6ab7f4ad9387982ea4fd948fd254ff8d
vanthang240803 <nguyenthang240803@gmail.com>
2024-11-17 19:03:42 +0700

    Add Global Module

--- src/modules/me/me.module.ts
+++ src/modules/me/me.module.ts
@@ -1,20 +1,13 @@
 import { Logger, Module } from "@nestjs/common";
 import { MeController } from "./me.controller";
 import { MeService } from "./me.service";
-import { CommonModule } from "@/common/common.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { JwtModule } from "@nestjs/jwt";
-import * as entities from "@/database/entities";
-import { AuthenticationService } from "@/common";
-import { JwtStrategy } from "@/common/strategies";
+import { User, Role } from "@/database/entities";
 
 @Module({
-  imports: [
-    CommonModule,
-    TypeOrmModule.forFeature(Object.values(entities)),
-    JwtModule,
-  ],
+  imports: [TypeOrmModule.forFeature([User, Role]), JwtModule],
   controllers: [MeController],
-  providers: [MeService, AuthenticationService, JwtStrategy, Logger],
+  providers: [MeService, Logger],
 })
 export class MeModule {}
--- src/modules/options/options.module.ts
+++ src/modules/options/options.module.ts
@@ -1,20 +1,12 @@
 import { Logger, Module } from "@nestjs/common";
 import { OptionsService } from "./options.service";
-import { CommonModule } from "@/common/common.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { JwtModule } from "@nestjs/jwt";
-import * as entities from "@/database/entities";
-import { JwtStrategy } from "@/common/strategies";
-import { AuthenticationService } from "@/common";
+import { Product, Option } from "@/database/entities";
 import { OptionsController } from "./options.controller";
 
 @Module({
-  imports: [
-    CommonModule,
-    TypeOrmModule.forFeature(Object.values(entities)),
-    JwtModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Product, Option])],
   controllers: [OptionsController],
-  providers: [OptionsService, AuthenticationService, JwtStrategy, Logger],
+  providers: [OptionsService, Logger],
 })
 export class OptionsModule {}
